dbfa1cbfbb8bffd8cf6a5838152bed3a6e5470ae
W4TR1X <W4TR1X@users.noreply.github.com>
2020-05-31 21:13:04 +0300

    Add project files.

---
+++ MarkovChain/Form1.Designer.vb
@@ -0,0 +1,179 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.txtRefText = New System.Windows.Forms.TextBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.txtResult = New System.Windows.Forms.TextBox()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtRefText
+        '
+        Me.txtRefText.Location = New System.Drawing.Point(12, 12)
+        Me.txtRefText.Multiline = True
+        Me.txtRefText.Name = "txtRefText"
+        Me.txtRefText.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtRefText.Size = New System.Drawing.Size(601, 371)
+        Me.txtRefText.TabIndex = 0
+        Me.txtRefText.Text = resources.GetString("txtRefText.Text")
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(118, 388)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {50, 0, 0, 0})
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {2, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(53, 20)
+        Me.NumericUpDown1.TabIndex = 1
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {10, 0, 0, 0})
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 391)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(105, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Kontrol Sıralaması (1)"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(171, 386)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 24)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Hesapla (2)"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(770, 387)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 24)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Oluştur (4)"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'txtResult
+        '
+        Me.txtResult.Location = New System.Drawing.Point(619, 12)
+        Me.txtResult.Multiline = True
+        Me.txtResult.Name = "txtResult"
+        Me.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtResult.Size = New System.Drawing.Size(598, 371)
+        Me.txtResult.TabIndex = 5
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.Location = New System.Drawing.Point(717, 389)
+        Me.NumericUpDown2.Maximum = New Decimal(New Integer() {600, 0, 0, 0})
+        Me.NumericUpDown2.Minimum = New Decimal(New Integer() {10, 0, 0, 0})
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(53, 20)
+        Me.NumericUpDown2.TabIndex = 1
+        Me.NumericUpDown2.Value = New Decimal(New Integer() {400, 0, 0, 0})
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(621, 391)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(92, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Karakter Sayısı (3)"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(259, 386)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(354, 23)
+        Me.ProgressBar1.TabIndex = 6
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(964, 391)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(103, 13)
+        Me.LinkLabel1.TabIndex = 7
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Text Sample Source"
+        '
+        'LinkLabel2
+        '
+        Me.LinkLabel2.AutoSize = True
+        Me.LinkLabel2.Location = New System.Drawing.Point(1088, 391)
+        Me.LinkLabel2.Name = "LinkLabel2"
+        Me.LinkLabel2.Size = New System.Drawing.Size(129, 13)
+        Me.LinkLabel2.TabIndex = 8
+        Me.LinkLabel2.TabStop = True
+        Me.LinkLabel2.Text = "Markov Chain (Wikipedia)"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1229, 422)
+        Me.Controls.Add(Me.LinkLabel2)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.txtResult)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.NumericUpDown2)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.txtRefText)
+        Me.Name = "Form1"
+        Me.Text = "Markov Chain"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtRefText As TextBox
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents txtResult As TextBox
+    Friend WithEvents NumericUpDown2 As NumericUpDown
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents LinkLabel2 As LinkLabel
+End Class
---
+++ MarkovChain/Form1.vb
@@ -0,0 +1,99 @@
+﻿Public Class Form1
+
+    Public sequence As New Dictionary(Of String, List(Of String))
+    Dim sampleSize As Integer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        CreateSequence()
+    End Sub
+
+    Sub CreateSequence()
+        sequence.Clear()
+
+        txtRefText.Text = txtRefText.Text.Replace(vbCrLf, " ").Replace(vbNewLine, " ").Replace(vbCr, " ").Replace("  ", " ")
+
+        If txtRefText.TextLength = 0 Then
+            Exit Sub
+        End If
+
+        Dim refText As String = txtRefText.Text
+        sampleSize = NumericUpDown1.Value
+
+        ProgressBar1.Maximum = refText.Length
+        ProgressBar1.Value = 0
+        ProgressBar1.Step = 1
+
+        For i As Integer = 0 To refText.Length - 1 - sampleSize
+
+            Dim sample As String = refText.Substring(i, sampleSize)
+            Dim nextChar As String = refText.Substring(i + sampleSize, 1)
+
+            If Not sequence.Where(Function(x) (x.Key = sample)).Any Then
+                sequence.Add(sample, updateStringList(Nothing, nextChar))
+            Else
+                sequence.Item(sample) = updateStringList(sequence.Item(sample), nextChar)
+            End If
+
+            ProgressBar1.PerformStep()
+            Application.DoEvents()
+        Next
+
+        ProgressBar1.Value = 0
+
+    End Sub
+
+    Function updateStringList(l As List(Of String), text As String) As List(Of String)
+        If l Is Nothing Then
+            l = New List(Of String)
+            l.Add(text)
+        Else
+            If Not l.Contains(text) Then
+                l.Add(text)
+            End If
+        End If
+
+        Return l
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        txtResult.Clear()
+
+        Dim maxChars As Integer = NumericUpDown2.Value
+
+        Dim wordList As List(Of String) = txtRefText.Text.Split(" ").Distinct.ToList
+
+        Dim search As String = wordList(Rnd() * (wordList.Count - 1))
+
+        Do While search.Length < sampleSize OrElse search.First = Char.ToLower(search.First)
+            search = wordList(Rnd() * (wordList.Count - 1))
+        Loop
+        search = search.Substring(0, sampleSize)
+
+
+        Dim result As String = search
+
+        For i As Integer = 0 To maxChars - 1
+
+            If Not sequence.ContainsKey(search) Then Continue For
+
+            Dim sList As List(Of String) = sequence.Item(search)
+
+            result &= sList.Item(Rnd() * (sList.Count - 1))
+
+            search = result.Substring(result.Length - sampleSize, sampleSize)
+
+        Next
+
+        txtResult.AppendText(result)
+
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("https://defnesumanblogs.com/category/turkce-yazilar/")
+    End Sub
+
+    Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        Process.Start("https://tr.wikipedia.org/wiki/Markov_zinciri")
+    End Sub
+End Class
